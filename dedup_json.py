88c72665fea8eecc7b033f0a6aa2aeca84f00129
faustusdotbe <simon.hengchen@gmail.com>
2023-05-04 08:37:58 +0200

    cleaning dates

--- dedup_json.py
+++ dedup_json.py
@@ -2,6 +2,7 @@ import json
 from pprint import pprint
 import sys
 from tqdm import tqdm
+import re
 
 
 def try_set(dic):
@@ -25,10 +26,19 @@ def dedup(d):
             
     return d2
 
+def clean_dates(d):
+    for i, v in enumerate(d):
+        for i2, entry in enumerate(v["entries"]):
+            dates = re.findall("\d{4}", entry["year"])
+            d[i]["entries"][i2]["year"] = dates
+    return d
+        
+
 with open("entries.json") as f:
     d = json.load(f)
     
 d2 = dedup(d)
+d3 = clean_dates(d2)
 with open("entries_clean.json", "w") as f:
-    json.dump(d2, f, indent=4, ensure_ascii=False)
+    json.dump(d3, f, indent=4, ensure_ascii=False)
 print("Dedup JSON saved at entries_clean.json")
