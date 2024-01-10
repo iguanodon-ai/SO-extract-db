d54f820332cb9f0ad9f5e322b24b30d9454fa668
faustusdotbe <simon.hengchen@gmail.com>
2024-01-10 12:02:28 +0100

    e

---
+++ src/types.d.ts
@@ -0,0 +1,64 @@
+import { Selectable } from "kysely";
+
+type Definition = {
+  description: string;
+  subtitle: string; // To rename
+  examples: string[];
+  year: string | number;
+  subDefinitions: Definition[];
+};
+
+type Entry = {
+  key: number; // ort_snr
+  word: string; // ortografie
+  nature: string;
+  definitions: Definition[];
+};
+
+export interface Database {
+  superlemma: SuperlemmaTable;
+  lemma: LemmaTable;
+  betydelser: BetydelserTable;
+  etymologier: EtymologierTable;
+  syntex: SyntexTable;
+}
+
+export interface SuperlemmaTable {
+  s_nr: number;
+  ordklass: string;
+  typ: string;
+}
+
+export interface LemmaTable {
+  s_nr: number;
+  wtype: string;
+  origin: string;
+  ortografi: string;
+}
+
+export interface BetydelserTable {
+  s_nr: number;
+  x_nr: number;
+  kc_nr: number;
+  def: string;
+  deft: string;
+  typ: string;
+  kcorder: number;
+}
+
+export interface EtymologierTable {
+  x_nr: number;
+  fbel: string;
+  etymologi: string;
+}
+
+export interface SyntexTable {
+  kc_nr: number;
+  sx_text: string;
+}
+
+export type SuperLemma = Selectable<SuperlemmaTable>;
+export type Lemma = Selectable<LemmaTable>;
+export type Betydelser = Selectable<BetydelserTable>;
+export type Etymologier = Selectable<EtymologierTable>;
+export type Syntex = Selectable<SyntexTable>;
